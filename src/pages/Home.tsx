eea1e8c9e9d086eaccbc6f3fee001534eb17ffcb
Pedro Jose Bertelli <pedro.bertelli@coreon.com.br>
2023-06-06 18:48:12 -0300

    Criação das rotas.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Flex, Text } from "@chakra-ui/react";
+import React from "react";
+
+export default function Home() {
+  return (
+    <Flex>
+      <Text>teste</Text>
+    </Flex>
+  );
+}
